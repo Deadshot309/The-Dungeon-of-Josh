cdcdb46809442aedad54bf2a4f014bc66156c1a2
Deadshot309 <200111@hkis.edu.hk>
2016-05-19 10:09:31 +0800

    asdf

--- gameTemplate1/gamedev.js
+++ gameTemplate1/gamedev.js
@@ -1,5 +1,27 @@
 $( document ).ready(function() {
+	$("#main").click(function() {
+			
+			function leftArrowPressed() {
+            var element = document.getElementById("Person.gif");
+            element.style.left = parseInt(element.style.left) - 5 + 'px';
+            }
 
+            function rightArrowPressed() {
+            var element = document.getElementById("Person.gif");
+            element.style.left = parseInt(element.style.left) + 5 + 'px';
+
+            }
+
+            function upArrowPressed() {
+            var element = document.getElementById("Person.gif");
+            element.style.top = parseInt(element.style.top) - 5 + 'px';
+            }
+
+            function downArrowPressed() {
+            var element = document.getElementById("Person.gif");
+            element.style.top = parseInt(element.style.top) + 5 + 'px';
+            }
+	}}
 //Your jquery goes here
 
 });
